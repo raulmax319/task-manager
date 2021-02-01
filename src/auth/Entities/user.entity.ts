e69ccfab80dd8173a36758915b4cc2f8e86e5cfe
Raul Max <raulmax319@gmail.com>
2021-02-01 19:07:22 -0300

    Corrected unique decorator

--- src/auth/Entities/user.entity.ts
+++ src/auth/Entities/user.entity.ts
@@ -1,6 +1,14 @@
-import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  BaseEntity,
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+  Unique,
+} from 'typeorm';
 
 @Entity()
+@Unique(['email'])
+@Unique(['username'])
 export class User extends BaseEntity {
   @PrimaryGeneratedColumn()
   id: number;
