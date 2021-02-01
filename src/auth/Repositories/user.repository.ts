3dc5d927e83910e9a94372120cc7739eba355350
Raul Max <raulmax319@gmail.com>
2021-02-01 19:05:14 -0300

    Create account validations/exceptions

--- src/auth/Repositories/user.repository.ts
+++ src/auth/Repositories/user.repository.ts
@@ -1,3 +1,7 @@
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { EntityRepository, Repository } from 'typeorm';
 import { AuthCredentialsDto } from '../dto/auth-credentials.dto';
 import { User } from '../Entities/user.entity';
@@ -12,8 +16,18 @@ export class UserRepository extends Repository<User> {
     user.email = email;
     user.password = password;
 
-    console.log('account creation was successful.');
+    try {
+      await user.save();
+      console.log('account creation was successful.');
+    } catch (err) {
+      const [detail] = err.detail.split(' ').splice(1);
+      // eslint-disable-next-line prefer-const
+      let [key, name] = detail.split('=');
+      key.match(/(?=.*email)/) ? (key = 'Email') : (key = 'Username');
 
-    await user.save();
+      if (err.code === '23505')
+        throw new ConflictException(`${key} ${name} already exists.`);
+      else throw new InternalServerErrorException();
+    }
   }
 }
